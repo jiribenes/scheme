df90b116b5243dad9542e1d9567a95b2c233ed15
Jiří Beneš <mail@jiribenes.com>
2017-03-23 10:16:42 +0100

    Added comments to scheme.c, removed vector tests

--- src/scheme.c
+++ src/scheme.c
@@ -5,44 +5,45 @@
 #include "write.h"
 
 // write to stdout with newline
-void test_write(value_t val) {
+static void test_write(value_t val) {
     write(stdout, val);
     puts("");
 }
 
 void test(vm_t *vm) {
-    test_write(NIL_VAL);
+    test_write(NIL_VAL); // ()
 
     value_t num = NUM_VAL(42);
-    test_write(num);
+    test_write(num); // 42
     
     cons_t *cons = cons_new(vm);
     cons->car = num;
-    test_write(PTR_VAL(cons));
+    test_write(PTR_VAL(cons)); // (42)
 
     cons->cdr = NUM_VAL(45);
-    test_write(PTR_VAL(cons));
+    test_write(PTR_VAL(cons)); // (42 . 45)
 
     cons_t *other = cons_new(vm);
     other->cdr = PTR_VAL(cons);
     other->car = NUM_VAL(43);
-    test_write(PTR_VAL(other));
+    test_write(PTR_VAL(other)); // (43 42 . 45)
 
     string_t *str = string_new(vm, "test", 4);
-    test_write(PTR_VAL(str));
+    test_write(PTR_VAL(str)); // "test"
 
     cons->car = PTR_VAL(str);
-    test_write(PTR_VAL(other));
-
-    vector_t *vec = vector_new(vm, 1);
-    vec->data[0] = num;
-    test_write(PTR_VAL(vec));
-
-    vector_insert(vm, vec, NUM_VAL(14), 1);
-    test_write(PTR_VAL(vec));
+    test_write(PTR_VAL(other)); // (43 "test" . 45)
 
     symbol_t *sym = symbol_new(vm, "lambda", 6);
-    test_write(PTR_VAL(sym));
+    test_write(PTR_VAL(sym)); // lambda
+
+    other = together(vm, PTR_VAL(other), PTR_VAL(sym));
+    test_write(PTR_VAL(other)); // (43 . lambda)
+    
+    cons_t *c = together(vm, NUM_VAL(14), NIL_VAL);
+    c = together(vm, NUM_VAL(15), PTR_VAL(c));
+    c = together(vm, NUM_VAL(16), PTR_VAL(c));
+    test_write(PTR_VAL(c)); // (14 15 16)
 }
 
 int main(void) {
