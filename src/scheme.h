a347836ee997c57b6524cb2d7b01b2cbe6f2ad67
Jiří Beneš <mail@jiribenes.com>
2017-03-26 00:55:56 +0100

    Added more comments, this time to vm.h and scheme.h

--- src/scheme.h
+++ src/scheme.h
@@ -5,11 +5,15 @@
 
 typedef struct vm_t vm_t;
 
+// creates a new vm
 vm_t *vm_new();
+// frees a vm
 void vm_free(vm_t *vm);
 
+// (re)allocates a pointer
 void *vm_realloc(vm_t *vm, void* ptr, size_t old_size, size_t new_size);
 
+// garbage collect
 void gc(vm_t *vm);
 
 #endif // _scheme_h
--- src/vm.h
+++ src/vm.h
@@ -8,12 +8,18 @@
 
 #define MAX_NUM_TEMP 256
 
+
 struct vm_t {
-    ptrvalue_t *head; // most recent allocated
+    // a linked list of all allocated values
+    ptrvalue_t *head;
 
+    // total size of all allocated values
     size_t allocated;
-    size_t gc_threshold; // allocated needed to trigger gc
+    // the size of allocated values
+    // to trigger the garbage collector
+    size_t gc_threshold;
 
+    // a table of all symbols (needed for interning)
     symbol_t *symbol_table;
 
     env_t *env;
@@ -21,15 +27,22 @@ struct vm_t {
 
     value_t curval;
     
+    // a stack of temporary roots
+    // these are values, that shouldn't be deleted by the gc
     size_t num_temp;
     ptrvalue_t *temp[MAX_NUM_TEMP];
 };
 
+// adds a primitive function under name [name] to env
 void primitive_add(vm_t *vm, env_t *env, const char *name, size_t len, primitive_fn fn);
+// adds a variable to env
 void variable_add(vm_t *vm, env_t *env, symbol_t *sym, value_t val);
 
+// evaluates a list
 value_t eval_list(vm_t *vm, env_t *env, value_t list);
+// evaluates a value
 value_t eval(vm_t *vm, env_t *env, value_t val);
+// evaluates all arguments in [val] and returns the latest value
 value_t begin(vm_t *vm, env_t *env, value_t val);
 
 void vm_push_temp(vm_t *vm, ptrvalue_t *ptr);
