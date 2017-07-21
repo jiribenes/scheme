abefa16890565a79318c90dbd1f7aa38bea8b2a1
Jiří Beneš <mail@jiribenes.com>
2017-07-21 20:00:30 +0200

    Added 'foldl', 'foldr', 'filter', 'reduce' to stdlib

--- src/stdlib.scm
+++ src/stdlib.scm
@@ -7,10 +7,10 @@
 
     (define pair? cons?)
     (define true #t)
-    (define null ())
+    (define null '())
     (define false #f)
     (define (null? x)
-        (eq? x ()))
+        (eq? x '()))
 
     (define (bool? x)
         (if (eq? x #f)
@@ -24,12 +24,37 @@
 
     (define (map fn lst)
         (if (null? lst)
-            ()
+            '()
             (cons (fn (car lst))
                 (map fn (cdr lst)))))
 
+    (define (filter fn lst)
+        (if (null? lst)
+            '()
+            (if (fn (car lst))
+                (cons (car lst)
+                      (filter fn (cdr lst)))
+                (filter fn (cdr lst)))))
+
+    (define (foldl fn acc lst)
+        (if (null? lst)
+            acc
+            (foldl fn
+                   (fn acc (car lst))
+                   (cdr lst))))
+
+    (define (foldr fn acc lst)
+        (if (null? lst)
+            acc
+            (fn (car lst)
+                (foldr fn acc (cdr lst)))))
+
+    (define (reduce fn lst)
+        (foldl fn (car lst) (cdr lst)))
+    
     (define (test a b)
         (write
             (eq? a b)))
+
 
     'stdlib)
