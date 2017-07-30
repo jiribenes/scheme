9a5cda3154073de8fcd82c81140f6052ae8a77b3
Jiří Beneš <mail@jiribenes.com>
2017-07-30 16:44:24 +0200

    Added tests for variadic functions and lambdas

---
+++ test/func/variadic.scm
@@ -0,0 +1,18 @@
+(begin
+    (define (list-test . args) args)
+    (test (equal? (list-test 1 2 3 4 5) '(1 2 3 4 5)) #t)
+    (test (equal? (list-test) '()) #t)
+
+    (define (tail x . y) y)
+    (test (equal? (tail 1 2 3 4 5) '(2 3 4 5)) #t)
+    (test (equal? (tail 1 2) '(2)) #t)
+    
+    (define (head x . y) x)
+    (test (equal? (head 1 2 3 4 5) 1) #t)
+    (test (equal? (head '(1 2 3 4) 5) '(1 2 3 4)) #t)
+    
+    (define (snd x y . z) y)
+    (test (equal? (snd 1 2 3 4 5) 2) #t)
+    (test (equal? (snd "aaa" 'sym "bbb" '(1 2 3 4 5)) 'sym) #t)
+    (test (equal? (snd '(1 2) '(3 4) '(5 6) 7 8 "end") '(3 4)) #t)
+)
---
+++ test/func/variadic_lambda.scm
@@ -0,0 +1,6 @@
+(begin
+    (test ((lambda args (reduce + args)) 1 2 3 4 5) 15)
+    (test (equal? ((lambda (head . tail) tail) 1 2 3 4 5) '(2 3 4 5)) #t)
+    (test (equal? ((lambda (head . tail) head) 1 2 3 4 5) 1) #t)
+    (test (equal? ((lambda (fst snd . rest) snd) 1 2 3 4 5) 1) #t)
+)
